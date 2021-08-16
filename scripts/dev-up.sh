7ee02335c1fe4e9db0d1df3d7343e27e3624144f
v.yemelianov <v.yemelianov@voverc.com>
2021-08-16 20:50:40 +0300

    fixed message issue

--- scripts/dev-up.sh
+++ scripts/dev-up.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # Starts all the services passed as arguments
-docker-compose --project-name na -f nats-cluster.yml up --no-start --build $@
-docker stack deploy --compose-file nats-cluster.yml --with-registry-auth na-ts
+docker-compose --project-name na -f na-ts.yml up --no-start --build $@
+docker stack deploy --compose-file na-ts.yml --with-registry-auth na-ts
