82c10a5a0edeefe8d31de09fb2b997faf21f27e2
v.yemelianov <v.yemelianov@voverc.com>
2021-08-27 21:56:56 +0300

    NATS - copy nats.conf

--- scripts/swarm-update.sh
+++ scripts/swarm-update.sh
@@ -1,4 +1,6 @@
 wget -q $1 -O docker-stack-na-ts.yml
+echo "Copy nats.conf"
+wget -q $2 -O ~/na/nats.conf
 echo "Deploying new na-ts stack"
 docker stack rm na-ts
 docker stack deploy --compose-file docker-stack-na-ts.yml --with-registry-auth na-ts
