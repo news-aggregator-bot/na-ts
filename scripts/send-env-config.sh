166176132cf7160adb7ce3d90f1861bfb5c81f36
v.yemelianov <v.yemelianov@voverc.com>
2021-08-27 21:47:27 +0300

    NATS - added send envs

---
+++ scripts/send-env-config.sh
@@ -0,0 +1,10 @@
+cd ~/.ssh
+#echo "SendEnv MYSQL_NA_DB" >> tempconfig
+#echo "SendEnv MYSQL_NA_USER" >> tempconfig
+#echo "SendEnv MYSQL_NA_PASSWORD" >> tempconfig
+#echo "SendEnv SPRING_PROFILES_ACTIVE" >> tempconfig
+value=$(<config)
+echo "$value" >> tempconfig
+cp tempconfig config
+cat tempconfig
+rm tempconfig
